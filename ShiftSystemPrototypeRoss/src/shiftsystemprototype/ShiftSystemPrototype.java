7cd1e202a5ad51852364504f89b7b98052e84362
Fofiscus <ross.reid93@gmail.com>
2014-09-10 20:33:39 +0100

    Added shift System prototype

---
+++ ShiftSystemPrototypeRoss/src/shiftsystemprototype/ShiftSystemPrototype.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shiftsystemprototype;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author x12483858
+ */
+public class ShiftSystemPrototype {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ShiftGUI GUI = new ShiftGUI();
+        GUI.setVisible(true);
+        
+        
+     
+    }
+}
